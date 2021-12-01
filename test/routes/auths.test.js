0adc1ee7f129361895d3390129716016b23a90cd
Jorge Rodrigues <a20632@alunos.ipca.pt>
2021-12-01 14:49:48 +0000

    Coding the auth tests #1 to #5

--- test/routes/auths.test.js
+++ test/routes/auths.test.js
@@ -2,12 +2,60 @@ const request = require('supertest');
 
 const app = require('../../src/app');
 
-test.skip('Test #1 - Recieve token when authenticating ', () => { });
+test.skip('Test #1 - Create user with signup', () => {
+  const email = `${Date.now()}@onboard.com`;
+  return request(app).post('/auth/signup')
+    .send({
+      company_id: '1', name: 'Jorge Rodrigues', email, password: '123456', role_id: '1',
+    })
+    .then((res) => {
+      expect(res.status).toBe(201);
+      expect(res.body.name).toBe('Jorge Rodrigues');
+      expect(res.body).toHaveProperty('email');
+      expect(res.body).not.toHaveProperty('password');
+    });
+});
 
-test.skip('Test #2 - Wrong authentication attempt with wrong password', () => { });
+test.skip('Test #2 - Recieve token when authenticating ', () => {
+  const email = `${Date.now()}@onboard.com`;
+  return app.services.user.save(
+    {
+      company_id: '1', name: 'Jorge Rodrigues', email, password: '123456', role_id: '1',
+    },
+  ).then(() => request(app).post('/auth/signin')
+    .send({ email, password: '123456' }))
+    .then((res) => {
+      expect(res.status).toBe(200);
+      expect(res.body).toHaveProperty('token');
+    });
+});
 
-test.skip('Test #3 - Wrong authentication attempt with wrong user', () => { });
+test.skip('Test #3 - Wrong authentication attempt with wrong password', () => {
+  const email = `${Date.now()}@onboard.com`;
+  return app.services.user.save(
+    {
+      company_id: '1', name: 'Jorge Rodrigues', email, password: '123456', role_id: '1',
+    },
+  ).then(() => request(app).post('/auth/signin')
+    .send({ email, password: '654321' }))
+    .then((res) => {
+      expect(res.status).toBe(400);
+      expect(res.body.error).toBe('Invalid user or password');
+    });
+});
 
-test.skip('Test #4 - Access to protected routes', () => { });
+test.skip('Test #4 - Wrong authentication attempt with wrong user', () => {
+  return request(app).post('/auth/signin')
+    .send({ email: 'doesnotexist@mail.com', password: '654321' })
+    .then((res) => {
+      expect(res.status).toBe(400);
+      expect(res.body.error).toBe('Invalid user or password');
+    });
+});
 
-test.skip('Test #5 - Create user', () => { });
+test.skip('Test #5 - Access to protected routes', () => {
+  return request(app).get('/v1/users')
+    .then((res) => {
+      expect(res.status).toBe(401);
+    });
+});
