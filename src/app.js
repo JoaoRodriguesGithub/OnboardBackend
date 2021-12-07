705c04e6089f23e4385a57a12a06eca98e21ebbc
JR-LAPTOP01\JR <a20629@alunos.ipca.pt>
2021-12-07 21:22:19 +0000

    Coding passport logic, setting up the routes and implementing the error handling functions on app

--- src/app.js
+++ src/app.js
@@ -7,13 +7,26 @@ const knexfile = require('../knexfile');
 app.db = knex(knexfile.test);
 
 consign({ cwd: 'src', verbose: false })
-  .include('./config/middlewares.js')
+  .include('./config/passport.js')
+  .then('./config/middlewares.js')
+  .then('./services')
   .then('./routes')
   .then('./config/routes.js')
   .into(app);
 
 app.get('/', (req, res) => {
   res.status(200).send();
+});
+
+app.use((err, req, res, next) => {
+  const { name, message, stack } = err;
+  if (name === 'ValidationError') res.status(400).json({ error: message });
+  else if (name === 'ForbiddenError') res.status(403).json({ error: message });
+  else {
+    console.log(message);
+    res.status(500).json({ name, message, stack });
+  }
+  next(err);
 });
 
 module.exports = app;
